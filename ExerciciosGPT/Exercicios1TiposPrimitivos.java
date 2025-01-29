991f156f2453707033eb6ba432e5ff157a26cb7d
iamyago <yagodev10@gmail.com>
2025-01-29 03:56:16 -0300

    Java
    Exercicios propostos pelo chatGPT em java Basico.

---
+++ ExerciciosGPT/Exercicios1TiposPrimitivos.java
@@ -0,0 +1,17 @@
+package ExerciciosGPT;
+
+public class Exercicios1TiposPrimitivos {
+    public static void main(String[] args) {
+        int valor = 10;
+        double valor1 = 1.000;
+        float valor3 = 12.5F;
+        char valor4 = 'A';
+        boolean valor5 = true;
+        System.out.println("Valor do int: " + valor);
+        System.out.println("Valor do double: " + valor1);
+        System.out.println("Valor do float: " + valor3);
+        System.out.println("Valor do char: " + valor4);
+        System.out.println("Valor do boolean: " + valor5);
+
+    }
+}
