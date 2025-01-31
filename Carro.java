a9662cf999867e6fed84f96670aac39030c81762
iamyago <yagodev10@gmail.com>
2025-01-31 18:05:16 -0300

    Delete Carro.java

--- Carro.java
+++
@@ -1,7 +0,0 @@
-package academy.devjojo.maratonajava.javacore.Aintroducaoclasses.dominio;
-
-public class Carro {
-    public String nomecarro;
-    public String modelocarro;
-    public int ano;
-}
