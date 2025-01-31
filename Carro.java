0ea877423f0dab788d1e232432fd95a63582ce28
iamyago <yagodev10@gmail.com>
2025-01-31 18:05:02 -0300

    JAVA

---
+++ Carro.java
@@ -0,0 +1,7 @@
+package academy.devjojo.maratonajava.javacore.Aintroducaoclasses.dominio;
+
+public class Carro {
+    public String nomecarro;
+    public String modelocarro;
+    public int ano;
+}
