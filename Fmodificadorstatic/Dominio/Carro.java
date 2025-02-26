c52ada2822508096ca1ae5a1adda4b877b433ea7
Yago Erik <yagodev10@gmail.com>
2025-02-26 17:49:37 -0300

    POO

---
+++ Fmodificadorstatic/Dominio/Carro.java
@@ -0,0 +1,40 @@
+package academy.devjojo.maratonajava.javacore.Fmodificadorstatic.Dominio;
+
+public class Carro {
+    private String nome;
+    private double velocidadeMaxima;
+    public static double velocidadeLimite = 250;
+
+    // propriedade q é independente do objeto
+    // pertence a classe e nao mais ao objeto
+    // o atributo pertence a classe e os objetos compartilham do mesmo valor.
+
+    public Carro(String nome, double velocidadeMaxima) {
+        this.nome = nome;
+        this.velocidadeMaxima = velocidadeMaxima;
+
+    }
+
+    public void imprime() {
+        System.out.println("------");
+        System.out.println("Nome" + this.nome);
+        System.out.println("Velo max" + this.velocidadeMaxima);
+        System.out.println("Velo limite" + Carro.velocidadeLimite);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getVelocidadeMaxima() {
+        return velocidadeMaxima;
+    }
+
+    public void setVelocidadeMaxima(double velocidadeMaxima) {
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+}
