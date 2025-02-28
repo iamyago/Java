0fb5eb64b78412260a00a95bd4c7971aba320646
Yago Erik <yagodev10@gmail.com>
2025-02-28 16:00:55 -0300

    .

--- Fmodificadorstatic/Dominio/Anime.java
+++ Fmodificadorstatic/Dominio/Anime.java
@@ -2,14 +2,17 @@ package academy.devjojo.maratonajava.javacore.Fmodificadorstatic.Dominio;
 
 public class Anime {
     private String nome;
-    private int[] episodios;
+    private static int[] episodios;
 
-    {
+    static {
+        System.out.println("bloco de incialização");
         episodios = new int[100];
         for (int i = 0; i < episodios.length; i++) {
             episodios[i] = i+1;
         }
     }
+
+    // 0 - bloco de inicialização é executado quando a JVM carregar a classe
     // 1 - Alocado espaço em memoria pro objeto
     // 2 - cada atributo de classe é criado e incializado com valores default ou o que for passada
     // 3 - bloco de incialização é executado
@@ -18,6 +21,11 @@ public class Anime {
     // sintax usando CHAVES.
     // executado antes dos construtores
     // executado todas as vezes que vc cria um objeto
+    // bloco de inicialização estatico é executado apenas uma vez quando a classe é carregada pelo JVM, isso acontece antes do espaço ser alocado em memoria
+    // executado apenas uma vez
+    // não pode acessar atributos de instancia de um metodo estatico.
+
+
     public Anime(String nome){
         this.nome = nome;
     }
@@ -25,7 +33,7 @@ public class Anime {
     public Anime() {
 
         System.out.println(episodios);
-        for(int episodios: this.episodios){
+        for(int episodios: Anime.episodios){
             System.out.println(episodios + "");
         }
         System.out.println();
---
+++ Fmodificadorstatic/Teste/AnimeTeste01.java
@@ -0,0 +1,12 @@
+package academy.devjojo.maratonajava.javacore.Fmodificadorstatic.Teste;
+
+import academy.devjojo.maratonajava.javacore.Fmodificadorstatic.Dominio.Anime;
+
+public class AnimeTeste01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime();
+        Anime anime2 = new Anime();
+        Anime anime3 = new Anime();
+
+    }
+}
