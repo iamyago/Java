133a8f311175397b3150c257ded429fd0ad54aba
Yago Erik <yagodev10@gmail.com>
2025-04-14 15:58:33 -0300

    Add files via upload

---
+++ Hheranca/dominio/endereco.java
@@ -0,0 +1,22 @@
+package academy.devjojo.maratonajava.javacore.Hheranca.dominio;
+
+public class endereco {
+    private String rua;
+    private String Cep;
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getCep() {
+        return Cep;
+    }
+
+    public void setCep(String cep) {
+        Cep = cep;
+    }
+}
