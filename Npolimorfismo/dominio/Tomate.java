0b459ab19ca0586acc324b345cd145ba214441d0
Yago <yagodev10@gmail.com>
2025-06-03 17:09:12 -0300

    Lpoo

--- Npolimorfismo/dominio/Tomate.java
+++ Npolimorfismo/dominio/Tomate.java
@@ -2,8 +2,17 @@ package academy.devjojo.maratonajava.javacore.Npolimorfismo.dominio;
 
 public class Tomate extends Produto{
     public static final double IMPOSTO_POR_CENTO = 0.06;
+    private String dataValidade;
     public Tomate(String nome, double valor) {
         super(nome, valor);
+    }
+
+    public String getDataValidade() {
+        return dataValidade;
+    }
+
+    public void setDataValidade(String dataValidade) {
+        this.dataValidade = dataValidade;
     }
 
     @Override
---
+++ Npolimorfismo/teste/ProdutoTeste02.java
@@ -0,0 +1,10 @@
+package academy.devjojo.maratonajava.javacore.Npolimorfismo.teste;
+
+import academy.devjojo.maratonajava.javacore.Npolimorfismo.dominio.Computador;
+import academy.devjojo.maratonajava.javacore.Npolimorfismo.dominio.Produto;
+
+public class ProdutoTeste02 {
+    public static void main(String[] args) {
+        Produto produto = new Computador("M4", 2000);
+    }
+}
