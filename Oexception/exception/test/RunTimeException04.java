1ba7d9712e0b2f3c14422a1a5b23ae57d46473e0
Yago <yagodev10@gmail.com>
2025-07-01 15:56:47 -0300

    LPOO

---
+++ Oexception/exception/test/RunTimeException04.java
@@ -0,0 +1,31 @@
+package academy.devjojo.maratonajava.javacore.Oexception.exception.test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
+
+public class RunTimeException04 {
+    public static void main(String[] args) {
+        try {
+            throw new ArrayIndexOutOfBoundsException();
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("dentro do ArrayIndexOutOfBoundsException ");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("dentro do IndexOutOfBoundsException");
+        } catch (IllegalArgumentException e) {
+            System.out.println("dentro do IllegalArgumentException");
+        } catch (ArithmeticException e) {
+            System.out.println("dentro do IllegalArgumentException");
+        } catch (RuntimeException e) {
+            System.out.println("Dentro do RuntimeException ");
+        }
+        try {
+            talvezLanceException();
+        } catch (SQLException | FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+    private static void talvezLanceException() throws SQLException, FileNotFoundException {
+
+    }
+}
