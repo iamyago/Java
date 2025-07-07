f080f6d1d02c466d07040082e1bf1a235e32cee4
Yago <yagodev10@gmail.com>
2025-07-07 16:51:15 -0300

    Lpoo

---
+++ Pwrappers/test/WrappersTest01.java
@@ -0,0 +1,25 @@
+package academy.devjojo.maratonajava.javacore.Pwrappers.test;
+
+public class WrappersTest01 {
+    public static void main(String[] args) {
+        byte byteP = 1;
+        short shortP = 1;
+        int intP = 10;
+        float floatP = 10F;
+        double doubleP = 10D;
+        char charP = 'W';
+        boolean booleanP = false;
+
+        // ctrL + F
+        //ctrl + R
+
+        Byte byteW = 1;
+        Short shortW = 1;
+        Integer intW = 10;
+        Float floatW = 10F;
+        Double doubleW = 10D;
+        Character charW = 'W';
+        Boolean booleanW = false;
+
+    }
+}
