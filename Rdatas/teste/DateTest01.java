d83e3d5f86cb0635af8b8f9759674eda86035f9b
Yago <yagodev10@gmail.com>
2025-07-22 15:11:39 -0300

    LPOO

---
+++ Rdatas/teste/DateTest01.java
@@ -0,0 +1,10 @@
+package academy.devjojo.maratonajava.javacore.Rdatas.teste;
+
+import java.util.Date;
+
+public class DateTest01 {
+    public static void main(String[] args) {
+        Date date = new Date(1753207393096L); // long - 1 de janeiro de 1974 - 100000
+        System.out.println(date);
+    }
+}
