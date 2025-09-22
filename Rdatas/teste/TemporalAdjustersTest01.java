5d57d1e463d398b73a1302e0b74eed7b4ee84cae
Yago <yagodev10@gmail.com>
2025-09-22 16:03:50 -0300

    LPOO
    Classes Utilitarias

--- Rdatas/teste/TemporalAdjustersTest01.java
+++ Rdatas/teste/TemporalAdjustersTest01.java
@@ -8,6 +8,7 @@ import java.time.temporal.TemporalAdjusters;
 
 public class TemporalAdjustersTest01 {
     public static void main(String[] args) {
+        // manipula as datas de forma direta.
         LocalDate now = LocalDate.now();
         now = now.with(ChronoField.DAY_OF_MONTH,20);
         System.out.println(now);
