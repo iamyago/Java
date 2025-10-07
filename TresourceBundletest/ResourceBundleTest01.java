b7f882639990a48e06a1cc91a514fe3daa817821
Yago <yagodev10@gmail.com>
2025-10-07 15:43:19 -0300

    LPOO

--- TresourceBundletest/ResourceBundleTest01.java
+++ TresourceBundletest/ResourceBundleTest01.java
@@ -2,10 +2,16 @@ package academy.devjojo.maratonajava.javacore.TresourceBundletest;
 
 import java.time.LocalDate;
 import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class ResourceBundleTest01 {
     public static void main(String[] args) {
         // ResourceBundle é uma forma do java pegar uma mensagem baseada no local.
+        // puxa do arquivo fora do src.
         System.out.println(Locale.getDefault());
+        ResourceBundle bundle = ResourceBundle.getBundle("message", new Locale("pt", "BR"));
+        boolean naruto = bundle.containsKey("naruto");
+        System.out.println(naruto);
+
     }
 }
