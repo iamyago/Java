d11d383bd544072c8db4616fa93f12e7fbe6ac90
Yago <yagodev10@gmail.com>
2025-10-23 15:31:44 -0300

    Regex

---
+++ Uregex/PatterMatcherTest04.java
@@ -0,0 +1,41 @@
+ package academy.devjojo.maratonajava.javacore.Uregex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatterMatcherTest04 {
+    public static void main(String[] args) {
+        // regex é uma Linguagem que usa metaCaracteres para encontrar padrões no texto.
+        // \d = Todos os digitos numericos.
+        // \D tudo o que não for digito
+        // \s espaços em branco \t \n \f \r
+        // \S caractere que não é branco
+        // \w caracteres de palavras a-z A-Z, digitos e _
+        // \W tudo o que não for caractere de palavra
+        // [] são usados para criar uma classe de caracteres, ou seja, definem um conjunto de caracteres dos quais você deseja encontrar qualquer um deles em uma determinada posição da string
+        // quantificadores são caracteres que te dão poder em pegar determinada expressão baseado no que o metacaracter representa
+        // ? zero ou uma
+        // * zero ou mais
+        // + uma ou mais
+        // ()
+        // $
+        // {n,m} de n até m
+         int numerohex = 0x59f86A;
+        System.out.println(numerohex);
+        String regex = "0[xX]([0-9a-f])+\\s";
+        String texto = "abaaba";
+    //  String texto2 = "12 0x 0X 0xFFABC 0x109 0x1";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(texto);
+        System.out.println("texto: " +texto);
+        System.out.println("indice: 0123456789" );
+        System.out.println("Regex: " +regex);
+        System.out.println("Posicoes encontradas");
+        while (matcher.find()) {
+            System.out.print(matcher.start()+" "+ matcher.group()+ "\n");
+        }
+
+    }
+
+
+}
