e1a75b7e3385202886a5c24044fdec17ad3b62ad
Yago <yagodev10@gmail.com>
2025-12-09 15:03:34 -0300

    LPOO

---
+++ Vio/test/BufferReaderTest01.java
@@ -0,0 +1,19 @@
+package academy.devjojo.maratonajava.javacore.Vio.test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BufferReaderTest01 {
+    public static void main(String[] args) {
+        File file = new File("file.txt");
+        try (FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader (fr)){
+            br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
